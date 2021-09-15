1b909bbefc5ccc1079f66dfcb2b3ae08976f0bbd
mahdi lotfi <74452648+mahdilotfi167@users.noreply.github.com>
2021-09-15 21:41:13 +0430

    add some ui and index page

---
+++ src/main/java/co/live/trip/controller/MainController.java
@@ -0,0 +1,12 @@
+package co.live.trip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
--- src/main/java/co/live/trip/security/SecurityConfig.java
+++ src/main/java/co/live/trip/security/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/", "/css/**").permitAll().anyRequest().authenticated();
         http.formLogin().loginPage("/user/login").permitAll();
         http.logout().permitAll();
     }
